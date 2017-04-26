4b993cd51a334b34efdd2009d987392e036fae0d
Joe Herbert <joeherbert@users.noreply.github.com>
2017-04-26 13:58:07 +0100

    Update SocialFeedControllerExtension.php

--- code/SocialFeedControllerExtension.php
+++ code/SocialFeedControllerExtension.php
@@ -16,13 +16,34 @@ class SocialFeedControllerExtension extends DataExtension
 	{
 		$combinedData = array();
 		if ($customHandleInstagram) {
-			$combinedData = $this->getProviderFeed(SocialFeedProviderInstagram::get()->filter('Enabled', 1), $combinedData, $customHandleInstagram);
+			if(preg_match("/\/(\d+)$/",$customHandleInstagram,$matches)) {
+			  $handle=$matches[1];
+			}
+			else
+			{
+			  $handle=$customHandleInstagram;
+			}
+			$combinedData = $this->getProviderFeed(SocialFeedProviderInstagram::get()->filter('Enabled', 1), $combinedData, $handle);
 		}
 		if ($customHandleFacebook) {
-			$combinedData = $this->getProviderFeed(SocialFeedProviderFacebook::get()->filter('Enabled', 1), $combinedData, $customHandleFacebook);
+			if(preg_match("/\/(\d+)$/",$customHandleFacebook,$matches)) {
+			  $handle=$matches[1];
+			}
+			else
+			{
+			  $handle=$customHandleFacebook;
+			}
+			$combinedData = $this->getProviderFeed(SocialFeedProviderFacebook::get()->filter('Enabled', 1), $combinedData, $handle);
 		}
 		if ($customHandleTwitter) {
-			$combinedData = $this->getProviderFeed(SocialFeedProviderTwitter::get()->filter('Enabled', 1), $combinedData, $customHandleTwitter);
+			if(preg_match("/\/(\d+)$/",$customHandleTwitter,$matches)) {
+			  $handle=$matches[1];
+			}
+			else
+			{
+			  $handle=$customHandleTwitter;
+			}
+			$combinedData = $this->getProviderFeed(SocialFeedProviderTwitter::get()->filter('Enabled', 1), $combinedData, $handle);
 		}
 		$result = new ArrayList($combinedData);
 		$result = $result->sort('Created', 'DESC');
