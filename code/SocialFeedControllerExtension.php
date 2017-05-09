fdeffdbb5b3fe496863cf9b1acb469d4d2ee34b5
Joe Herbert <joeherbert@users.noreply.github.com>
2017-05-09 14:54:47 +0100

    Update SocialFeedControllerExtension.php

--- code/SocialFeedControllerExtension.php
+++ code/SocialFeedControllerExtension.php
@@ -17,21 +17,21 @@ class SocialFeedControllerExtension extends DataExtension
 		
 		$combinedData = array();
 		if ($customHandleInstagram) {
-			$link_array = explode('/',$customHandleInstagram);
+			$link_array = explode('/',rtrim($customHandleInstagram,'/'));
     		$handle = end($link_array);
     		$query_array = explode('?',$handle);
     		$handle = $query_array[0];
 			$combinedData = $this->getProviderFeed(SocialFeedProviderInstagram::get()->filter('Enabled', 1), $combinedData, $handle);
 		}
 		if ($customHandleFacebook) {
-			$link_array = explode('/',$customHandleFacebook);
+			$link_array = explode('/',rtrim($customHandleFacebook,'/'));
     		$handle = end($link_array);
     		$query_array = explode('?',$handle);
     		$handle = $query_array[0];
 			$combinedData = $this->getProviderFeed(SocialFeedProviderFacebook::get()->filter('Enabled', 1), $combinedData, $handle);
 		}
 		if ($customHandleTwitter) {
-			$link_array = explode('/',$customHandleTwitter);
+			$link_array = explode('/',rtrim($customHandleTwitter,'/'));
     		$handle = end($link_array);
     		$query_array = explode('?',$handle);
     		$handle = $query_array[0];
